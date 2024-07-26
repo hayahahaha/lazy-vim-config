70ae29c0f771f1452cdd1cfde276c34b4feb8fd8
Dean (Duy) Hoang <duy.hm@cyberlogitec.com>
2024-07-26 10:16:23 +0700

    refactor: package

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,30 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-mocha",
+  --   },
+  -- },
+  --
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("catppuccin-latte")
+  --   end,
+  -- },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "Johan-Palacios/onedarker",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd("colorscheme onedarker")
+    end,
   },
 }
